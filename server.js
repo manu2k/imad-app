4149407fa4e925abe84bbbd05299f9930873348f
MM <manu2k@gmail.com>
2017-08-10 11:02:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/article-one',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
+app.get('/article-one',function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+app.get('/article-one',function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
+
 app.get('/article-two',function(req,res){
     res.send('article2 file is requested and will be served here');
 });

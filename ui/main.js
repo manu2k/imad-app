8fd2d16d0a59d484f4066edbfb654a61dcb342cc
MM <manu2k@gmail.com>
2017-08-10 22:28:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,3 +14,19 @@ function moveRight(){
 img.onclick = function(){
     var interval = setInterval(moveRight,50);
 };
+
+var button = document.getElementById('counter');
+button.onclick(function (){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.Done) {
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementsById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET',"http://manu2k.imad.hasura-app.io/counter",true);
+    request.send(null);
+});

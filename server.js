79e31e65336f02a743e6310376ad50e45fb690a4
MM <manu2k@gmail.com>
2017-08-11 11:38:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,9 +122,9 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function (req,res){
+app.get('/submit-name', function (req,res){
     //get the names from the request
-    var name =req.params.name;
+    var name =req.query.name;
     
     names.push(name);
     

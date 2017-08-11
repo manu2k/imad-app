ccce30f9f556d3ca03d9ff2f5c9067a06d017465
MM <manu2k@gmail.com>
2017-08-11 10:52:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,8 +19,9 @@ var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function (){
     var request = new XMLHttpRequest();
+    
     request.onreadystatechange = function(){
-        if (request.readyState === XMLHttpRequest.Done) {
+        if (request.readyState === XMLHttpRequest.DONE) {
             if(request.status === 200){
                 var counter = request.responseText;
                 var span = document.getElementById('count');

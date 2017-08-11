bfe3bfd62500ac1876ea0d212fbea95cc38fa385
MM <manu2k@gmail.com>
2017-08-11 10:35:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,8 +17,8 @@ img.onclick = function(){
 
 var button = document.getElementById('counter');
 counter = 0;
-button.onclick(function (){
+button.onclick = function (){
    counter = counter + 1;
    var span = document.getElementById('count');
    span.innerHTML = counter.toString();
-});
+};

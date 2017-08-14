9e663e97b13561445e8505078593b950d5157efe
MM <manu2k@gmail.com>
2017-08-14 15:16:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -148,8 +148,8 @@ app.get('/:articleName',function(req,res){
 });*/
 
 app.get('/articles/:articleName',function(req,res){
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+//var articleName = req.params.articleName;
+   // res.send(createTemplate(articles[articleName]));
     pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err,result){
         if (err){
             res.status(500).send(err.toString());

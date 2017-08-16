9a88ec131522940959c7e1b1a098301d093dfb68
MM <manu2k@gmail.com>
2017-08-16 19:04:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-app.use(session({
+/*app.use(session({
     secret: "SomeRandomSecretValue",
     cookie:{maxAge: 1000*60*60*24*30}
-}));
+}));*/
 /*
 var articles = {
     'article-one' :{
@@ -165,14 +165,14 @@ app.post('/login',function(req,res){
         }
     });
 });
-
+/*
 app.get('/check-login',function(req,res){
     if(req.session&&req.session.auth&&req.session.auth.userId){
         res.send("You are logged in: " + req.session.auth.userId.toString());
     }else{
         res.send("You are not logged in!");
     }
-});
+});*/
 
 app.get('/logout',function(req,res){
     delete req.session.auth;

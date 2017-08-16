43750cc5e979061fc49e00b761aed64e38245824
MM <manu2k@gmail.com>
2017-08-16 12:10:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,6 +116,18 @@ app.get('/hash/:input',function(req,res){
   
 });
 
+app.get('/create-user',function(req,res){
+    var salt = crypto.getRandomBytes(128).toString('hex');
+    var dbString = hash(password, salt);
+    pool.query('INSERT INTO "user" (username, password) values($1,$2)',[username,dbstring],function(err,result){
+        if (err){
+         res.status(500).send(err.toString());
+        } else {
+         res.send("User successfully created: " + username);
+        }
+    });
+});
+
 var pool = new Pool(config);
 
 app.get('/test-db', function (req, res) {

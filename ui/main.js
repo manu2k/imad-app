52b71a4a793b924732f533358521cf4cdbf7fc31
MM <manu2k@gmail.com>
2017-08-16 13:42:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -95,3 +95,30 @@ enterComments.onclick = function(){
     request.open('GET','http://manu2k.imad.hasura-app.io/comments',true);
     request.send(null);
 };*/
+
+//submit username password
+var submitLogin = document.getElementById('login-btn');
+submitLogin.onclick = function (){
+var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200){
+                console.log("user logged in");
+                alert('logged in succesfully!');
+            } else if(request.status === 403){
+                alert('username/password is invalid');
+            } else if(request.status === 500){
+                alert('Something went wrong in the server');
+            }
+            
+        }
+    };
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://manu2k.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+};

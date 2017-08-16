99406dce8582004c3ed3743a93691bdc915b4699
MM <manu2k@gmail.com>
2017-08-16 19:23:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-/*app.use(session({
+app.use(session({
     secret: "someRandomSecretValue",
     cookie:{maxAge: 1000*60*60*24*30}
-}));*/
+}));
 /*
 var articles = {
     'article-one' :{

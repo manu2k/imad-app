03666cb65f278480f1f27842c888d0c1585ec80d
MM <manu2k@gmail.com>
2017-08-16 12:23:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'manu2k',
@@ -14,6 +15,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.user(bodyParser.json());
 
 /*
 var articles = {
@@ -116,7 +118,10 @@ app.get('/hash/:input',function(req,res){
   
 });
 
-app.get('/create-user',function(req,res){
+app.post('/create-user',function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+    
     var salt = crypto.getRandomBytes(128).toString('hex');
     var dbString = hash(password, salt);
     pool.query('INSERT INTO "user" (username, password) values($1,$2)',[username,dbstring],function(err,result){

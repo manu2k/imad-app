79c305eaf04bf63af0b557803c04905760e7549a
MM <manu2k@gmail.com>
2017-08-17 09:56:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -154,7 +154,7 @@ app.post('/login',function(req,res){
                 var hashedPassword = hash(password, salt);
                 if(hashedPassword === dbString){
                     //set the session
-                    //req.session.auth = {userId: result.rows[0].id};
+                    req.session.auth = {userId: result.rows[0].id};
                     
                     res.send('Credentials are correct!');
                 } else {
